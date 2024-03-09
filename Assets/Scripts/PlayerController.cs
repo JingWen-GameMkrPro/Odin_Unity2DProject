1b744dbe335eaf7fc09816872c342836da6fce4f
JingWen <jingwenhuang1008@gmail.com>
2024-03-09 20:50:58 +0900

    Add: Player Permission & State

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.Android;
+//using UnityEngine.Windows;
 
 public class PlayerController : MonoBehaviour
 {
@@ -15,24 +18,31 @@ public class PlayerController : MonoBehaviour
     public float gravity;
 
     [Flags]
-    public enum CharacterControllState
+    public enum CharacterPermission
     {
         None = 0,
         CanMove = 1<<0,
         CanJump = 1<<1,
     }
-    private CharacterControllState currentControllState = CharacterControllState.CanMove | CharacterControllState.CanJump;
+    private CharacterPermission currentPermission = CharacterPermission.CanMove | CharacterPermission.CanJump;
+    private Dictionary<CharacterPermission, Coroutine> lastSetTimer = new (); //用於管理目前權限封鎖倒數狀態
+
+    [Flags]
+    public enum CharacterState
+    {
+        None = 0,
+        isMove = 1 << 0,
+        isGrounded = 1 << 1,
+    }
+    private CharacterState currentState = CharacterState.isMove | CharacterState.isGrounded;
 
 
-
-    //Input
+    //Move
     private float horizontalInput;
-
-    //Direction
     private Vector3 horizontalMoveDirection;
 
     //Jump
-    private bool isGrounded = false;
+    //private bool isGrounded = false;
     
 
 
@@ -44,12 +54,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        Debug.Log("Current Permission State: " + Convert.ToString((int)currentState, 2).PadLeft(2, '0'));
+
+        //隨時確認是否在地板
         CheckGrounded();
 
+        //水平移動輸入
         HorizontalMove();
 
-        //按下跳躍健
-        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        //跳躍輸入，如果isGrounded = 1才行
+        if (Input.GetKeyDown(KeyCode.W) && (currentState & CharacterState.isGrounded) !=0)
         {
             Jump();
         }
@@ -58,23 +73,26 @@ public class PlayerController : MonoBehaviour
     //Horizon Move
     private void HorizontalMove()
     {
-        if ((currentControllState & CharacterControllState.CanMove) == 0) return;
+        //確認權限
+        if ((currentPermission & CharacterPermission.CanMove) == 0) return;
 
         horizontalInput = Input.GetAxis("Horizontal");
         horizontalMoveDirection = new Vector3(horizontalInput, 0f, 0f).normalized;
         transform.position += horizontalMoveDirection * horizontalMoveSpeed * Time.deltaTime;
     }
 
-
     private void CheckGrounded()
     {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.1f, groundLayer);
-        isGrounded = hit.collider != null;
+        bool isGrounded = hit.collider != null;
+        Debug.Log(isGrounded);
+        SetState(CharacterState.isGrounded, isGrounded);
     }
 
     private void Jump()
     {
-        if ((currentControllState & CharacterControllState.CanJump) == 0) return;
+        //確認權限
+        if ((currentPermission & CharacterPermission.CanJump) == 0) return;
 
         //按下按鈕
         //可以控制 "跳躍高度" & "重力加速度"
@@ -83,17 +101,72 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        //Check is Grounded
-        if (!isGrounded)
+        //確認是否在地板
+        if ((currentState | CharacterState.isGrounded) != 0)
         {
             rb.AddForce(Vector2.down * gravity);
         }
 
-        //Avoid velocity too fast, then overlap collider.
-        if(isGrounded && rb.velocity.y <0)
+        //當下墜後踩到地板的瞬間，必須盡快煞車，避免穿透地板
+        if((currentState | CharacterState.isGrounded) == 0 && rb.velocity.y <0)
         {
             rb.velocity = new Vector2(rb.velocity.x, 0f);
         }
+    }
+
+    //用於角色被封鎖權限之類的Debuff，像是移動、跳躍、攻擊
+    public void ClosePermissionForDuration(CharacterPermission inputPermission, int inputSeconds)
+    {
+        SetPermisssion(inputPermission, false);
+
+        //如果還沒被封鎖
+        if(!lastSetTimer.ContainsKey(inputPermission))
+        {
+            //新增倒數
+            lastSetTimer.Add(inputPermission, StartCoroutine(Countdown_Permission(inputPermission, inputSeconds)));
+        }
+        //如果正在被封鎖
+        else
+        {
+            //刷新倒數
+            StopCoroutine(lastSetTimer[inputPermission]);
+            lastSetTimer[inputPermission] = StartCoroutine(Countdown_Permission(inputPermission, inputSeconds));
+        }
+
+
+    }
+
+    //倒數計時
+    IEnumerator Countdown_Permission(CharacterPermission inputPermission, int inputSeconds = 1)
+    {
+        int currentSeconds = inputSeconds;
+        while(currentSeconds>0)
+        {
+            currentSeconds--;
+            yield return new WaitForSeconds(1);
+        }
+
+        //協程結束，刪除KEY值
+        if (lastSetTimer.ContainsKey(inputPermission)) lastSetTimer.Remove(inputPermission);
+        SetPermisssion(inputPermission, true);
+        yield return null;
+    }
+
+    //設定權限
+    private void SetPermisssion(CharacterPermission inputPermission, bool isOpen)
+    {
+        // 使用按位 OR 設定特定位元
+        if (isOpen) currentPermission |= inputPermission;
+        // 使用按位 AND 清除特定位元
+        else currentPermission &= ~inputPermission;
+    }
+
+    private void SetState(CharacterState inputState, bool isOpen)
+    {
+        // 使用按位 OR 設定特定位元
+        if (isOpen) currentState |= inputState;
+        // 使用按位 AND 清除特定位元
+        else currentState &= ~inputState;
     }
 
 }
