0c3a03b0b9c3f7e75c6caa20ebdf9abb35d8918e
Xinyu <liux30307@gmail.com>
2024-06-29 18:07:17 +0800

    GameTip: 遊戲提示文字系統(尚未將文字設定於中上位置以及黑底)

--- Assets/Scripts/GameTipSystemController.cs
+++ Assets/Scripts/GameTipSystemController.cs
@@ -1,25 +1,62 @@
 using System.Collections;
+using TMPro;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameTipSystemController : MonoBehaviour
 {
-
-    private void Start()
-    {
-        
-    }
-
     /// <summary>
     /// 以下為外界可以呼叫的API
     /// </summary>
     /// <param name="text">提示訊息</param>
     /// <param name="continueSecond">持續的秒數</param>
     /// <param name="canBeOverride">當持續時間未結束，會不會被新的訊息覆蓋</param>
-    public void ShowTip(string text, float continueSecond, bool canBeOverride)
+
+    public TMP_Text tiptext;
+    private float continueSecond = 3.0f;
+    private float timer;
+    private bool canBeOverride = false;
+
+    private void Start()
     {
-        //當呼叫此函式時，將會顯示提示訊息框物件 (應該設定visible，不要設定active，這會使腳本無法運作)
-        //此時將會進入倒數計時，時間結束後將會重新隱藏提示訊息框，unity計時功能請參閱 corountine
-        //您可以簡單自行在這邊的Start()呼叫此函式，看看是否可以出現文字並在時間後隱藏
+
+       
+
+    }
+
+
+
+    public void ShowTip(string text, float duration, bool canBeOverride)
+    {
+        // 當呼叫此函式時，將會顯示提示訊息框物件 (應該設定visible，不要設定active，這會使腳本無法運作)
+        // 此時將會進入倒數計時，時間結束後將會重新隱藏提示訊息框，unity計時功能請參閱 coroutine
+        // 您可以簡單自行在這邊的 Start() 呼叫此函式，看看是否可以出現文字並在時間後隱藏
+
+        tiptext.text = text;
+        tiptext.gameObject.SetActive(true);
+
+        this.continueSecond = duration;
+        this.canBeOverride = canBeOverride;
+
+        if (canBeOverride)
+        {
+            StopAllCoroutines();
+            timer = 0;
+        }
+
+    }
+
+    private void Update()
+    {
+        if (tiptext.gameObject.activeSelf && canBeOverride)
+        {
+            timer += Time.deltaTime;
+            if (timer >= continueSecond)
+            {
+                tiptext.gameObject.SetActive(false);
+                timer = 0;
+            }
+        }
     }
 }
